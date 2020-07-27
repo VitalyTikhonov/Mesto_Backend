fc1ed979e569f83bfc7332b2126d3c24097a55d6
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-07-27 20:28:24 +0400

    S12 C02 Added Express, Eslint, Nodemon

---
+++ app.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+app.listen(3000);
