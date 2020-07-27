e7bb774cfd7b242f266008d21b88ab20dd154235
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-07-27 20:52:21 +0400

    S12 C03 Added Public and opened it via Express

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 const express = require('express');
+const path = require('path');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 app.listen(3000);
+app.use(express.static(path.join(__dirname, 'public')));
